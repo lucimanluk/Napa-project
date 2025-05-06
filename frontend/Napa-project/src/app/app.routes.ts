921e2dd6d065ee32e947bd80d1be0e951e72bfe6
lucimanluk <lucimanluk4000@gmail.com>
2025-05-06 11:52:27 +0300

    Started adding routing and changing the color of the images.

--- frontend/Napa-project/src/app/app.routes.ts
+++ frontend/Napa-project/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { VoyagesComponent } from './voyages/voyages.component';
+import { CountriesComponent } from './countries/countries.component';
+import { PortsComponent } from './ports/ports.component';
+import { ShipsComponent } from './ships/ships.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'ships', component: ShipsComponent},
+    {path: 'countries', component: PortsComponent},
+    {path: 'voyages', component: VoyagesComponent},
+    {path: 'countries', component: CountriesComponent},
+];

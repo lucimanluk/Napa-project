8d7cbc7b63aa237b163732276ad1e7ab39c27615
LucianTrifan <luciant@admintools.io>
2025-05-06 09:42:03 +0300

    Created comnponents that will represnt the pages.

---
+++ frontend/Napa-project/src/app/ships/ships.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ships',
+  imports: [],
+  templateUrl: './ships.component.html',
+  styleUrl: './ships.component.css'
+})
+export class ShipsComponent {
+  
+}
---
+++ frontend/Napa-project/src/app/voyages/voyages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voyages',
+  imports: [],
+  templateUrl: './voyages.component.html',
+  styleUrl: './voyages.component.css'
+})
+export class VoyagesComponent {
+
+}

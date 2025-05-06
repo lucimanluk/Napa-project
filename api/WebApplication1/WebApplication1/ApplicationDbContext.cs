16d0eb1e272600505e783905aa3247dbf03c920c
lucimanluk <lucimanluk4000@gmail.com>
2025-05-06 23:45:41 +0300

    Started working on the apis and models for data.

---
+++ api/WebApplication1/WebApplication1/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ships> Ships { get; set; }
+    }
+}
---
+++ api/WebApplication1/WebApplication1/Models/Entities/Ships.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models.Entities
+{
+
+    public class Ships
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required int MaxSpeed { get; set; }
+    }
+}
